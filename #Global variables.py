f83316c551a539ea66b68055fbca4322bc57c226
Ellis <165180266+EX0d1A-D3Str0Y@users.noreply.github.com>
2024-10-16 19:02:57 +0000

    Global goo

---
+++ #Global variables.py
@@ -0,0 +1,10 @@
+#Global variables
+
+pass=0
+
+#Main code
+student=int(input("Enter score: "))
+if student>=pass:
+    print("Well done, you have passed the exam!")
+else:
+    print("Oh no! You did not pass!")
